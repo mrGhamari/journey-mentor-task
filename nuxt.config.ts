c7053e9fa5841d95db4d2175bf9154f351b3a663
mrGhamari <mmdrza77@gmail.com>
2025-10-19 22:26:46 +0330

    add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,15 @@ export default defineNuxtConfig({
     head: {
       htmlAttrs: { lang: 'en' },
       title: 'Sample Project',
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        {
+          rel: 'stylesheet',
+          href:
+            'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap',
+        },
+      ],
     },
   },
   components: {
